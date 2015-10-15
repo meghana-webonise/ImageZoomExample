b6a184ab600b86a7d20d2148d190342ea25b6064
meghana-webonise <meghana.shastri@weboniselab.com>
2015-10-15 15:40:27 +0530

    Working code of zoom in and out of image view

--- app/src/main/java/com/weboniselab/meghana/android/app/imagezoomexample/MainActivity.java
+++ app/src/main/java/com/weboniselab/meghana/android/app/imagezoomexample/MainActivity.java
@@ -1,17 +1,16 @@
 package com.weboniselab.meghana.android.app.imagezoomexample;
 
-import android.opengl.Matrix;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView mImageView;
+    /*ImageView mImageView;
     ScaleGestureDetector mScaleGestureDetector;
     android.graphics.Matrix mMatrix=new android.graphics.Matrix();
 
@@ -40,5 +39,89 @@ public class MainActivity extends AppCompatActivity {
             mImageView.setImageMatrix(mMatrix);
             return true;
         }
+    }*/
+
+    ImageView mImageView;
+    TextView mTextView;
+    Bitmap bitmap;
+    int bmpWidth, bmpHeight;
+    int touchState;
+    final int IDLE = 0;
+    final int TOUCH = 1;
+    final int PINCH = 2;
+    float dist0, distCurrent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView=(ImageView) findViewById(R.id.imageView);
+        mTextView = (TextView)findViewById(R.id.touchevent);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        bmpWidth = bitmap.getWidth();
+        bmpHeight = bitmap.getHeight();
+        distCurrent = 1;
+        dist0 = 1;
+        drawMatrix();
+        mImageView.setOnTouchListener(MyOnTouchListener);
+        touchState = IDLE;
+
     }
+
+    private void drawMatrix(){
+        float curScale = distCurrent/dist0;
+        if (curScale < 0.1){
+            curScale = 0.1f;
+        }
+
+
+        Bitmap resizedBitmap;
+        int newHeight = (int) (bmpHeight * curScale);
+        int newWidth = (int) (bmpWidth * curScale);
+        resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, false);
+        mImageView.setImageBitmap(resizedBitmap);
+    }
+
+    View.OnTouchListener MyOnTouchListener
+            = new View.OnTouchListener(){
+
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            float distx, disty;
+
+            switch(event.getAction() & MotionEvent.ACTION_MASK){
+                case MotionEvent.ACTION_DOWN:
+                    mTextView.setText("ACTION_DOWN");
+                    touchState = TOUCH;
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    mTextView.setText("ACTION_POINTER_DOWN");
+                    touchState = PINCH;
+                    distx = event.getX(0) - event.getX(1);
+                    disty = event.getY(0) - event.getY(1);
+                    dist0 = (float)Math.sqrt(distx * distx + disty * disty);
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    mTextView.setText("ACTION_MOVE");
+                    if(touchState == PINCH){
+                        distx = event.getX(0) - event.getX(1);
+                        disty = event.getY(0) - event.getY(1);
+                        distCurrent = (float)Math.sqrt(distx * distx + disty * disty);
+                        drawMatrix();
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    mTextView.setText("ACTION_UP");
+                    touchState = IDLE;
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    mTextView.setText("ACTION_POINTER_UP");
+                    touchState = TOUCH;
+                    break;
+            }
+
+            return true;
+        }
+
+    };
 }
